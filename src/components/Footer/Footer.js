e4e8dbe8f88861c5daccbb1fec721c416da64471
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-09-27 15:08:27 +0200

    fix: update Footer styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import './Footer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faAt } from '@fortawesome/free-solid-svg-icons';
