002db93d60c1458f227f99d183119c76f64e4e75
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-09-29 17:32:35 +0200

    refactor: change class to a function component

---
+++ src/providers/GithubAPI.js
@@ -0,0 +1,39 @@
+const GithubAPI = () => {
+	const token = process.env.REACT_APP_ACCESS_TOKEN;
+	const url = 'https://api.github.com';
+
+	const setHeaders = () => {
+		return {
+			headers: {
+				Accept: 'application/vnd.github+json',
+				Authorization: `token ${token}`,
+			},
+		};
+	};
+
+	const _fetch = (options, additionalPath = '') => {
+		const apiUrl = url + additionalPath;
+
+		return fetch(apiUrl, options).then((resp) => {
+			if (resp.ok) {
+				return resp.json();
+			}
+			return Promise.reject(resp);
+		});
+	};
+
+	const getUserInfo = (username) => {
+		return _fetch({ ...setHeaders() }, `/users/${username}`);
+	};
+
+	const getUserRepositories = (username) => {
+		return _fetch({ ...setHeaders() }, `/users/${username}/repos`);
+	};
+
+	return {
+		getUserInfo,
+		getUserRepositories,
+	};
+};
+
+export { GithubAPI };
