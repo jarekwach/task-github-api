774d16b75590d7eb46db8dc256da8c90c03cd0e0
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-10-01 18:18:23 +0200

    fix: update css styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,11 +8,7 @@ const Header = () => {
 	return (
 		<section className='header'>
 			<header className='header__container'>
-				<div className='header__content'>
-					<h1 className='header__title'>hello!</h1>
-					<h2 className='header__name'>I'm Jarek</h2>
-					<p className='header__info'>It's my GitHubApp</p>
-				</div>
+				<h1 className='header__title'>GitHub App</h1>
 				<div className='header__logo'>
 					<FontAwesomeIcon
 						className='header__ico'
